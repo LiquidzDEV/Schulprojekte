a96d5680a526ad7da08f71d183c4a9225d26ffa8
Liquidz <pascal.hobza@outlook.com>
2017-01-16 15:41:12 +0100

    Added Project Werkzeugverleih

---
+++ Werkzeugverleih/Werkzeug.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werkzeugverleih
+{
+    class Werkzeug
+    {
+        public string bezeichnung;
+        public int tageKurzausleihe;
+        public int preisKurzausleihe;
+        public int tageLangausleihe;
+        public int preisLangausleihe;
+
+        public Werkzeug(string bezeichnung, string tageKurzausleihe, string preisKurzausleihe, string tageLangausleihe, string preisLangausleihe)
+        {
+            this.bezeichnung = bezeichnung;
+            this.tageKurzausleihe = Int32.Parse(tageKurzausleihe);
+            this.preisKurzausleihe = Int32.Parse(preisKurzausleihe);
+            this.tageLangausleihe = Int32.Parse(tageLangausleihe);
+            this.preisLangausleihe = Int32.Parse(preisLangausleihe);
+        }
+
+    }
+}
