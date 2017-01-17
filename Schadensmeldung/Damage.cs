86bfce6b9dd6aeb34497e6bab60f9ad068d184a5
Liquidz <liquidz@liquidz.one>
2017-01-17 16:34:44 +0100

    Added Damage Class

--- Schadensmeldung/Damage.cs
+++ Schadensmeldung/Damage.cs
@@ -7,5 +7,17 @@ namespace Schadensmeldung
 {
     class Damage
     {
+        public int damageCategory;
+        public string device;
+        public string damage;
+        public int status;
+
+        public Damage(int damageCategory, string device, string damage, int status)
+        {
+            this.damageCategory = damageCategory;
+            this.device = device;
+            this.damage = damage;
+            this.status = status;
+        }
     }
 }
