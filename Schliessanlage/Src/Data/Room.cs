83223f928ad0aaed84acf6562b9685b5de77ffa2
Liquidz <liquidz@liquidz.one>
2017-02-03 19:00:04 +0100

    First Testing with Arduino connection

---
+++ Schliessanlage/Src/Data/Room.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Pascal "Liquidz" H.
+ * 03.02.2017 / 06:49
+ * 
+ * Description:
+ */
+using System;
+
+namespace Schliessanlage
+{
+	/// <summary>
+	/// Description of Room.
+	/// </summary>
+	public class Room
+	{
+		public int id { get; private set; }
+		public string roomnumber { get; private set; }
+		
+		public Room(int id, string roomnumber)
+		{
+			this.id = id;
+			this.roomnumber = roomnumber;
+		}
+	}
+}
---
+++ Schliessanlage/Src/Data/User.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Pascal "Liquidz" H.
+ * 03.02.2017 / 07:04
+ * 
+ * Description:
+ */
+using System;
+
+namespace Schliessanlage.Data
+{
+	/// <summary>
+	/// Description of User.
+	/// </summary>
+	public class User
+	{
+		public int id { get; private set; }
+		public int rfid { get; private set; }
+		public string vorname{ get; set; }
+		public string nachname{ get; set; }
+		
+		public User(int id, int rfid, string vorname, string nachname)
+		{
+			this.id = id;
+			this.rfid = rfid;
+			this.vorname = vorname;
+			this.nachname = nachname;
+		}
+	}
+}
